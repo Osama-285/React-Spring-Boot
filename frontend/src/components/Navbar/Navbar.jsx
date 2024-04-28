9288dc90e0e9e9de9ed1472aa26a50b5a38c7bb6
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-28 22:43:09 +0500

    navbar modifications

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
-      <div className="flex items-center space-x-4">
-        <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-          <li className="logo font-semibold text-grey-300 text-2xl"></li>
+      <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
+        <li className="logo font-semibold text-grey-300 text-2xl">Foods</li>
+      </div>
+      <div className="flex items-center space-x-2 lg:space-x-10">
+        <div>
+          <IconButton></IconButton>
         </div>
       </div>
     </div>
