9259a7ccbc1ac58b2cd50880d43453e9643669f8
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-28 22:37:34 +0500

    Creating navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between"></div>
+    <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
+      <div className="flex items-center space-x-4">
+        <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
+          <li className="logo font-semibold text-grey-300 text-2xl"></li>
+        </div>
+      </div>
+    </div>
   );
 };
 
