e9a712dfc01611e68dba55abaa8a7c44f206349a
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-29 18:53:03 +0500

    Navbar Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./Themes/DarkTheme";
 import { CssBaseline } from "@mui/material";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={darkTheme}>
+        <Navbar></Navbar>
         <CssBaseline />
       </ThemeProvider>
     </div>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
+import { Badge, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
@@ -8,7 +11,19 @@ const Navbar = () => {
       </div>
       <div className="flex items-center space-x-2 lg:space-x-10">
         <div>
-          <IconButton></IconButton>
+          <IconButton>
+            <SearchIcon sx={{ fontSize: "1.5rem" }} />
+          </IconButton>
+        </div>
+        <div className="">
+          <Avatar sx={{ bgcolor: "white", color: pink.A400 }}>O</Avatar>
+        </div>
+        <div className="">
+          <IconButton>
+            <Badge color="primary" badgeContent={3}>
+              <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
+            </Badge>
+          </IconButton>
         </div>
       </div>
     </div>
